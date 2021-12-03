e26743183da0f6c4a9d0777f45a6939a0dd689b2
Ilya Savitsky <ipsavitsky234@gmail.com>
2021-12-03 10:24:48 +0300

    health bar script

---
+++ Assets/Scripts/HealthBar/HealthBarHandler.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class HealthBarHandler : MonoBehaviour
+{
+
+    public float decay_time;
+    private float current_size;
+
+    private static Image knobComponent;
+    // Start is called before the first frame update
+    void Start()
+    {
+        current_size = decay_time;
+        knobComponent = GetComponent<Image>();
+        // Debug.Log("i have image component!");
+        InvokeRepeating("decreaseTime", 1.0f, 1.0f);
+    }
+
+    void decreaseTime()
+    {
+        current_size -= 1;
+        knobComponent.fillAmount = current_size / decay_time;
+    }
+}
